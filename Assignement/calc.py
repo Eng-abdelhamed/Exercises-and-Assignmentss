e218ce08baa15bc5bf42634648e27db8ce3ceff0
Abdelhamed Ahmed <168166326+Eng-abdelhamed@users.noreply.github.com>
2024-11-30 19:59:23 +0200

    calculator modes

---
+++ Assignement/calc.py
@@ -0,0 +1,14 @@
+# calc.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def Division(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+def Multiply(a, b):  # تعديل الخطأ الإملائي
+    return a * b
